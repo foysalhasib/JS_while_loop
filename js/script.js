07aacf90ff2fd61b34d6fca2d776ae97e3448e8d
Foysal Hasib <118272136+foysalhasib@users.noreply.github.com>
2024-02-16 22:51:11 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,57 @@
+//While_loop
+
+// var i = 1
+
+// while(i<=5) {
+//     console.log("Hello World " +i)
+//     i++
+// }
+
+
+// ===============================  Print 1 to 10  ====================================
+
+//  var number= 1
+
+//  while (number <=10) {
+    
+//        console.log(+number)
+//        number++
+//  }
+
+
+// ============================if condition=============================
+
+//  var number= 1
+
+//  while (number <=10) {
+//     if(number===5){
+//         console.log("Middle " +number )
+//     }
+//        console.log(+number)
+//        number++
+//  }
+
+
+
+// ============================ Sum of 1 to 10 =============================
+
+// var i= 1;
+// var sum=0;
+
+// while(i<=10) {
+//     sum = sum +i;
+//     i++;
+// }
+// console.log("Result is " +sum)
+
+
+// ========================= Multiplication of 1 to 10 =====================
+
+var i= 1;
+var  multiplication=1;
+
+while(i<=10) {
+    multiplication = multiplication * i;
+    i++;
+}
+console.log("Result is "  +multiplication)
